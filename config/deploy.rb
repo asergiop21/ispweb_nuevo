489d38f2d65650b3ed43b58722eb30785a81e182
Sergio <asergiop21@gmail.com>
2013-06-10 15:56:51 -0300

    arreglo deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,8 @@ namespace :deploy do
 end
     #desc "reload the database with seed data"
     task :seed do
-    run "cd #{current_path}; rake db:seed RAILS_ENV=#{rails_env}"
+      #run "cd #{current_path}; rake db:seed RAILS_ENV=#{rails_env}"
+      run "cd #{deploy_to}/current && bundle install vendor/gems"
 end
 end
 

0cb94a90dca6ccc1376ccf1580430acb48dc7f53
Sergio <asergiop21@gmail.com>
2013-07-20 10:24:43 -0300

    production devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  if Rails.env.production?
-    devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
-  else
     devise :database_authenticatable,  :recoverable, :rememberable, :trackable, :validatable
-  end
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :username,:lastname, :name, :username, :role_ids, :role_id, :current_password, :address, :cuit, :dni, :enabled

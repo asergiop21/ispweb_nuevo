4f26c695028224aeddc2f87021252f0c08f87fb7
Sergio <asergiop21@gmail.com>
2013-08-30 13:32:30 -0300

    whenever

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,6 +16,8 @@ role :db, 'proyect_ispweb.com', :primary => true # This is where Rails migration
 #role :db,  "your slave db-server here"
 
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+after ":deploy:update_crontab"
+
 namespace :deploy do
     task :start do ; end
     task :stop do ; end

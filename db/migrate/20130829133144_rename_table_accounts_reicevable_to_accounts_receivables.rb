78583310c0ac21d7db46437f3117c28cc715e28e
Sergio <asergiop21@gmail.com>
2013-08-30 12:30:58 -0300

    remove

--- db/migrate/20130829133144_rename_table_accounts_reicevable_to_accounts_receivables.rb
+++
@@ -1,9 +0,0 @@
-class RenameTableAccountsReicevableToAccountsReceivables < ActiveRecord::Migration
-  def self.up
-      rename_table :accounts_receivables, :account_receivables
-  end
-
-  def self.down
-      rename_table :account_receivables, :accounts_receivables
-  end
-end

3522874759c7fb922181f4665c1dc0af6c0d6437
Sergio <asergiop21@gmail.com>
2013-08-30 12:36:20 -0300

    Delete 20130829130025_rename_table_customer_account_to_customers_accounts.rb

--- db/migrate/20130829130025_rename_table_customer_account_to_customers_accounts.rb
+++
@@ -1,9 +0,0 @@
-class RenameTableCustomerAccountToCustomersAccounts < ActiveRecord::Migration
-  def self.up
-      rename_table :customers_accounts, :customer_payment_accounts
-  end
-
-  def  self.down
-      rename_table :customer_payment_accounts, :customers_accounts  
-  end
-end

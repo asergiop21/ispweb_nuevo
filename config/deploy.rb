b8308a75c22ff33e2a50343b3173bee830531496
Sergio <asergiop21@hotmail.com>
2014-06-23 15:12:22 -0300

    server deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,7 @@ set :application, "ispweb.com.ar"
 set :repository,  "https://github.com/asergiop21/ispweb.git"
 set :deploy_to, '/opt/isp/'
 set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+server 192.168.100.2
 set :user, 'server'
 set :deploy_via, :remote_cache
 set :use_sudo, false
